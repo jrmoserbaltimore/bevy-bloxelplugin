a46889a00673559c69cd66cb628a007316ebd97c
John Moser <john.r.moser@gmail.com>
2024-05-25 13:48:51 -0400

    Remove the dimension parameter

--- bloxel/src/lib.rs
+++ bloxel/src/lib.rs
@@ -8,20 +8,13 @@ use bevy_ecs::prelude::*;
 
 #[derive(Default)]
 pub struct BloxelPlugin {
-    dimension: f32,
 }
 
 impl Plugin for BloxelPlugin {
     fn build(&self, app: &mut App) {
-        // Default dimension is 1m cubes
-        self.dimension = 1;
     }
 }
 
 /// Returns this [`BloxelPlugin`] with the dimension set
 impl BloxelPlugin {
-    pub fn with_dimension(mut self, dimension: f32) {
-        self.dimension = dimension;
-        self
-    }
 }
